55ea1f5efc3ab1b12469e331e7c1555021270869
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-08-22 20:16:33 -0300

    wippp

---
+++ source/android/SonicVPad.hx
@@ -0,0 +1,102 @@
+package android;
+
+import flixel.FlxG;
+import flixel.graphics.frames.FlxTileFrames;
+import flixel.group.FlxSpriteGroup;
+import flixel.math.FlxPoint;
+import flixel.util.FlxDestroyUtil;
+import flixel.ui.FlxButton;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flash.display.BitmapData;
+import flixel.graphics.FlxGraphic;
+import openfl.utils.ByteArray;
+
+class FlxVirtualPad extends FlxSpriteGroup
+{
+	public var buttonA:FlxButton;
+	public var buttonB:FlxButton;
+	public var buttonC:FlxButton;
+	public var buttonY:FlxButton;
+	public var buttonX:FlxButton;
+	public var buttonLeft:FlxButton;
+	public var buttonUp:FlxButton;
+	public var buttonRight:FlxButton;
+	public var buttonDown:FlxButton;
+	public var dPad:FlxSpriteGroup;
+	public var actions:FlxSpriteGroup;
+
+	public function new(?DPad:FlxDPadMode, ?Action:FlxActionMode)
+	{
+		super();
+		scrollFactor.set();
+
+		if (DPad == null)
+			DPad = FULL;
+
+		if (Action == null)
+			Action = A_B_C;
+
+		dPad = new FlxSpriteGroup();
+		dPad.scrollFactor.set();
+
+		actions = new FlxSpriteGroup();
+		actions.scrollFactor.set();
+
+
+	}
+
+	override public function destroy():Void
+	{
+		super.destroy();
+
+		dPad = FlxDestroyUtil.destroy(dPad);
+		actions = FlxDestroyUtil.destroy(actions);
+
+		dPad = null;
+		actions = null;
+		buttonJump = null;
+		buttonPause = null;
+		buttonLeft = null;
+		buttonUp = null;
+		buttonDown = null;
+		buttonRight = null;
+	}
+
+	public function createButton(X:Float, Y:Float, Width:Int, Height:Int, Graphic:String, ?OnClick:Void->Void):FlxButton
+	{
+		var button = new FlxButton(X, Y);
+		var frame = getVirtualInputFrames().getByName(Graphic);
+		button.frames = FlxTileFrames.fromFrame(frame, FlxPoint.get(Width, Height));
+		button.resetSizeFromFrame();
+		button.solid = false;
+		button.immovable = true;
+		button.scrollFactor.set();
+
+		#if FLX_DEBUG
+		button.ignoreDrawDebug = true;
+		#end
+
+		if (OnClick != null)
+			button.onDown.callback = OnClick;
+
+		return button;
+	}
+
+	public static function getVirtualInputFrames():FlxAtlasFrames
+	{
+		#if !web
+		var bitmapData = new GraphicVirtualInput(0, 0);
+		#end
+			
+		#if !web
+		var graphic:FlxGraphic = FlxGraphic.fromBitmapData(bitmapData);
+		return FlxAtlasFrames.fromSpriteSheetPacker(graphic, Std.string(new VirtualInputData()));
+		#else
+		var graphic:FlxGraphic = FlxGraphic.fromAssetKey(Paths.image('sonicVPAD'));
+		return FlxAtlasFrames.fromSpriteSheetPacker(graphic, Std.string(new VirtualInputData()));
+		#end
+	}
+}
+
+@:keep @:bitmap("assets/android/sonicVPAD.png")
+class GraphicVirtualInput extends BitmapData {}
