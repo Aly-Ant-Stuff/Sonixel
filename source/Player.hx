72f2cd75b6a117817520138911c002e1fa01e575
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-08-26 01:03:41 -0300

    botei gravidade

--- source/Player.hx
+++ source/Player.hx
@@ -26,22 +26,20 @@ class Player extends FlxTypedSpriteGroup<FlxSprite>
 
 	public var spinDashT:FlxSprite;
 
-	public var pCam:FlxCamera;
 	public var curGround:FlxSprite;
-	public var camPos:FlxObject;
 	#if android
 	public var virtualPad:SonicVPad;
 	#end
-	public var animationStuff:Array<Dynamic> = [
-		["idle", [0], 12],
-		["lookUp", [1,2], 12],
-		["goDown", [3, 4], 12]
+	public var animationStuff:Array<Dynamic> = [ //name, frames, framerate, loop (true or false)
+		["idle", [0], 12, true],
+		["lookUp", [1,2], 12, false],
+		["goDown", [3, 4], 12, false]
 	];
 
 	//all the movement stuff
-	public var horiSPEED:Float = 0;
-	public var vertSPEED:Float = 0;
-	public var currentGRDSpeed:Float = 30;
+	public var xSpeed:Float = 0;
+	public var ySpeed:Float = 0;
+	public var currentGRDSpeed:Float = 0;
 	public var currentGRDAngle:Float = 0;
 
 	public var accelerationSpeed:Float = 0.046875;
@@ -49,12 +47,14 @@ class Player extends FlxTypedSpriteGroup<FlxSprite>
 	public var frictionSpeed:Float = 0.046875;
 	public var topSpeed:Float = 6;
 	public var gravityForce:Float = 0.21875;
-	public var direction:Int = 0;
+	public var airAccelerationSpeed:Float = 0.21875;
+	public var jumpForce:Float = 6.5;
 
 	//actions
 	public var hasLookedUp:Bool = false;
 	public var defaultCamPos:FlxPoint = FlxPoint.get(0, 0);
 	public var isGrounded:Bool = true;
+	public var isLookingUp:Bool = false;
 	public var isWalking:Bool = false;
 	public var isColliding:Bool = false;
 
@@ -69,74 +69,66 @@ class Player extends FlxTypedSpriteGroup<FlxSprite>
 			spr.animation.add(animationStuff[i][0], animationStuff[i][1], animationStuff[i][2]);
 		spr.animation.play("idle");
 		spr.antialiasing = false;
-		spr.scale.set(4, 4);
-		spr.updateHitbox();
 		add(spr);
 
-		hitbox = new FlxSprite(15 * 2, 8 * 2).makeGraphic(17 * 4, 40 * 4, FlxColor.fromRGB(255, 0, 255));
+		hitbox = new FlxSprite(15, 8).makeGraphic(17, 40, FlxColor.fromRGB(255, 0, 255));
 		hitbox.alpha = 0.4;
 		add(hitbox);
-
-		if (pCam != null && camPos != null)
-		{
-			pCam.follow(camPos, LOCKON, 1);
-		}
 	}
 
 	public function playerUpdate(elapsed:Float)
 	{
 		//player general part
-		for (s in [spr, hitbox])
-			s.updateHitbox();
-
-		defaultCamPos.set(spr.x + (spr.width / 2), spr.y + (spr.height / 2));
-		camPos.setPosition(defaultCamPos.x, defaultCamPos.y);
-		x += horiSPEED;
-		y += vertSPEED;
+		x += xSpeed;
+		y += ySpeed;
 
 		if (FlxG.keys.pressed.LEFT #if android || virtualPad.buttonLeft.pressed #end) {
 			spr.scale.x = -4;
-			if (horiSPEED > 0) {
-				horiSPEED -= decelerationSpeed;
-				if (horiSPEED <= 0)
-					horiSPEED = -0.5;
+			if (xSpeed > 0) {
+				xSpeed -= decelerationSpeed;
+				if (xSpeed <= 0)
+					xSpeed = -0.5;
 				direction = 1;
-			} else if (horiSPEED > -topSpeed) {
-				horiSPEED -= accelerationSpeed;
-				if (horiSPEED <= -topSpeed)
-					horiSPEED = -topSpeed;
+			} else if (xSpeed > -topSpeed) {
+				xSpeed -= accelerationSpeed;
+				if (xSpeed <= -topSpeed)
+				{
+					xSpeed = -topSpeed;
+					FlxG.log.add('sonic atinguiu a velocide maxima sem nada olha que fo');
+				}
 			}
 			isWalking = true;
 		}
 		else if (FlxG.keys.pressed.RIGHT #if android || virtualPad.buttonRight.pressed #end) {
 			spr.scale.x = 4;
-			if (horiSPEED < 0) {
-				horiSPEED += decelerationSpeed;
-				if (horiSPEED >= 0)
-					horiSPEED = 0.5;
-			} else if (horiSPEED < topSpeed) {
-				horiSPEED += accelerationSpeed;
-				if (horiSPEED >= topSpeed)
-					horiSPEED = topSpeed;
+			if (xSpeed < 0) {
+				xSpeed += decelerationSpeed;
+				if (xSpeed >= 0)
+					xSpeed = 0.5;
+			}else if (xSpeed < topSpeed){
+				xSpeed += accelerationSpeed;
+				if (xSpeed >= topSpeed) {
+					xSpeed = topSpeed;
+					FlxG.log.add('sonic atinguiu a velocide maxima sem nada olha que fo');
+				}
 			}
 		} else {
-			horiSPEED -= Math.min(Math.abs(horiSPEED), frictionSpeed) * FlxMath.signOf(horiSPEED);
+			xSpeed -= Math.min(Math.abs(xSpeed), frictionSpeed) * FlxMath.signOf(xSpeed);
 		}
 
-		if (FlxG.keys.pressed.UP #if android || virtualPad.buttonUp.pressed #end)
+		if (FlxG.keys.pressed.UP #if android || virtualPad.buttonUp.pressed #end && isGrounded)
 		{
 			spr.animation.play("lookUp");
-			new FlxTimer().start(2, function(t:FlxTimer) {
+			new FlxTimer().start(2, function(t:FlxTimer){
 				var targetY:Float = camPos.y - 100;
 				camPos.y -= 0.5;
 				hasLookedUp = true;
-				if (camPos.y <= targetY)
-				{
+				if (camPos.y <= targetY){
 					camPos.y = targetY;
 				}
 			});
-		} else {
-			if (hasLookedUp) {
+		}else{
+			if (hasLookedUp){
 				var targetY = defaultCamPos.y;
 				camPos.y += 0.5;
 				if (camPos.y >= targetY)
@@ -146,8 +138,23 @@ class Player extends FlxTypedSpriteGroup<FlxSprite>
 			}
 		}
 
-		FlxG.watch.addQuick("char speed in x", horiSPEED);
-		FlxG.watch.addQuick("char speed in y", vertSPEED);
+//  ----- gravity ----- //
+		if (!isGrounded){
+			ySpeed += gravityForce;
+			if (ySpeed >= 16) ySpeed = 16;
+		}
+
+		if (ySpeed < 0 && ySpeed > -4){
+			xSpeed -= ((xSpeed / 0.125) / 256);
+		}
+
+		if (FlxG.keys.pressed.SPACE #if android || virtualPad.buttonJump.pressed #end){
+			ySpeed -= jumpForce * Math.sin(currentGRDAngle);
+			xSpeed -= jumpForce * Math.cos(currentGRDAngle);
+		}
+
+		FlxG.watch.addQuick("velocidade do sonic em x", xSpeed);
+		FlxG.watch.addQuick("velocidade do sonic em y", ySpeed);
 	}
 
 	function indicesIterator(start:Int, end:Int):Array<Int> {
