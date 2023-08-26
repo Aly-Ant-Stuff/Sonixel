8a1fd6b6bc69330b0560d8c537b51a315f54a080
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-08-26 14:43:22 -0300

    update source/CoolestUtils.hx

--- source/CoolestUtils.hx
+++ source/CoolestUtils.hx
@@ -1,3 +1,15 @@
 package;
 
-class CoolestUtils {}
+class CoolestUtils {
+	public static function gravityCheck(p:Player, block:Block):Bool{ //for blocks
+		if (p != null && block != null){
+			if (!p.debugMode){
+				if (p.hitbox.y >= (block.hitbox.y - p.hitbox.height) && (p.hitbox.x >=block.hitbox.x && p.hitbox.x >=block.hitbox.x + block.hitbox.width)){
+					return true;
+				}else{
+					return false;
+				}
+			}
+		}
+	}
+}
