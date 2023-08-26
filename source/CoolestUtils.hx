89e29b7cff0b7c84c0c663b6b1da3e06214f315f
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-08-26 14:46:38 -0300

    no block class,,,,,

--- source/CoolestUtils.hx
+++ source/CoolestUtils.hx
@@ -4,7 +4,7 @@ class CoolestUtils {
 	public static function gravityCheck(p:Player, block:Block):Bool{ //for blocks
 		if (p != null && block != null){
 			if (!p.debugMode){
-				if (p.hitbox.y >= (block.hitbox.y - p.hitbox.height) && (p.hitbox.x >=block.hitbox.x && p.hitbox.x >=block.hitbox.x + block.hitbox.width)){
+				if (p.hitbox.y >= (block.hitbox.y - p.hitbox.height) && (p.hitbox.x >=block.hitbox.x && p.hitbox.x < block.hitbox.x + block.hitbox.width)){
 					return true;
 				}else{
 					return false;
