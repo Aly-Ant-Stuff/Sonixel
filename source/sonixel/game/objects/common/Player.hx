30374248a4ac44aede0e13bb1aafd90391b47715
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-08-26 22:33:10 -0300

    update source/sonixel/game/objects/common/Player.hx

--- source/sonixel/game/objects/common/Player.hx
+++ source/sonixel/game/objects/common/Player.hx
@@ -65,7 +65,7 @@ class Player extends FlxTypedSpriteGroup<FlxSprite>
 	{
 		super(x, y);
 
-		spr = new FlxSprite().loadGraphic(Paths.image('Sonic'), true, 46, 52);
+		spr = new FlxSprite().loadGraphic(Paths.image('Sonic'), true, 48, 48);
 		for (i in 0...animationStuff.length)
 			spr.animation.add(animationStuff[i][0], animationStuff[i][1], animationStuff[i][2]);
 		spr.animation.play("idle");
