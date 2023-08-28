b8a482f732053a2c9d637340e5b7e5dfe88ea27a
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-08-28 08:16:42 -0300

    update source/sonixel/util/CoolestUtils.hx

--- source/sonixel/util/CoolestUtils.hx
+++ source/sonixel/util/CoolestUtils.hx
@@ -8,11 +8,12 @@ class CoolestUtils {
 		var isColliding:Bool = false;
 		FlxG.collide(hitbox1, hitbox2, function(){
 			isColliding = true;
-			if(isColliding)
-				hitbox1.color = FlxColor.fromRGB(255, 0, 255);
-			else
-				hitbox1.color = hitbox1.originalColor;
 		});
+		if(isColliding)
+				hitbox1.color = FlxColor.fromRGB(255, 0, 255);
+		else
+				hitbox1.color = hitbox1.originalColor;
+
 		return isColliding;
 	}
 }
