25150d529afbc0d2eaba580a4702ba52f3aed4bf
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-08-28 10:01:43 -0300

    i could just make a variable for the color shit in coolestutils noo

--- source/sonixel/util/CoolestUtils.hx
+++ source/sonixel/util/CoolestUtils.hx
@@ -7,6 +7,7 @@ import sonixel.game.objects.misc.Hitbox;
 class CoolestUtils {
 	public static function collisionCheck(hitbox1:FlxSprite, hitbox2:FlxSprite, onCollision:Void->Void):Bool{
 		var isColliding:Bool = false;
+		var lastColor = hitbox1.color;
 		FlxG.collide(hitbox1, hitbox2, function(){
 			isColliding = true;
 			onCollision(); //ai acontece o resto
@@ -15,7 +16,7 @@ class CoolestUtils {
 			if(isColliding)
 					hitbox1.color = FlxColor.fromRGB(255, 0, 255);
 			else
-					hitbox1.color = hitbox1.originalColor;
+					hitbox1.color = lastColor;
 		}
 
 		return isColliding;
