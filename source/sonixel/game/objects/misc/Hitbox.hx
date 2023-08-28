fb3e9af1a98084aebcf6328b7404db0aed54cee4
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-08-28 08:06:57 -0300

    update source/sonixel/game/objects/misc/Hitbox.hx

--- source/sonixel/game/objects/misc/Hitbox.hx
+++ source/sonixel/game/objects/misc/Hitbox.hx
@@ -17,23 +17,26 @@ enum HitboxType
 
 class Hitbox extends FlxSprite
 {
+	private var coloringaMap:Map<HitboxType, FlxColor> = [
+		ATTACKABLE => FlxColor.RED,
+		HURT => FlxColor.RED,
+		HURT_AND_ATTACKABLE => FlxColor.RED,
+		REBOUND => FlxColor.BLUE,
+		VIEW_AREA => FlxColor.YELLOW,
+		PLAYER => FlxColor.GREEN,
+		TERRAIN => FlxColor.GREEN
+	];
 	public var type:HitboxType;
 	public var isColliding:Bool;
+	public var originalColor:FlxColor;
 
 	public function new (x:Float, y:Float, width:Int, height:Int, type:HitboxType){
 		super(x, y);
 		makeGraphic(width, height, FlxColor.WHITE); //white to be colorable
 
-		switch (type){
-			case ATTACKABLE | HURT | HURT_AND_ATTACKABLE:
-				color = FlxColor.RED;
-			case REBOUND:
-				color = FlxColor.BLUE;
-			case VIEW_AREA:
-				color = FlxColor.YELLOW;
-			case PLAYER | TERRAIN:
-				color = FlxColor.GREEN;
-		}
+		color = coloringaMap[type];
+		originalColor = coloringaMap[type];
+
 		type = this.type;
 		alpha = 0.4;
 	}
