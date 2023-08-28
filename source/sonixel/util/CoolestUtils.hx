89256b254ff2dfaac51327e991b200d93a7accd5
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-08-28 09:09:12 -0300

    update source/sonixel/util/CoolestUtils.hx

--- source/sonixel/util/CoolestUtils.hx
+++ source/sonixel/util/CoolestUtils.hx
@@ -1,18 +1,22 @@
 package sonixel.util;
 
-import sonixel.game.objects.misc.Hitbox;
 import flixel.FlxG;
+import flixel.FlxSprite;
+import sonixel.game.objects.misc.Hitbox;
 
 class CoolestUtils {
-	public static function collisionHitboxCheck(hitbox1:Hitbox, hitbox2:Hitbox):Bool{
+	public static function collisionCheck(hitbox1:FlxSprite, hitbox2:FlxSprite, onCollision:Void->Void):Bool{
 		var isColliding:Bool = false;
 		FlxG.collide(hitbox1, hitbox2, function(){
 			isColliding = true;
+			onCollision(); //ai acontece o resto
 		});
-		if(isColliding)
-				hitbox1.color = FlxColor.fromRGB(255, 0, 255);
-		else
-				hitbox1.color = hitbox1.originalColor;
+		if ((hitbox1 is Hitbox)){
+			if(isColliding)
+					hitbox1.color = FlxColor.fromRGB(255, 0, 255);
+			else
+					hitbox1.color = hitbox1.originalColor;
+		}
 
 		return isColliding;
 	}
