915caa367c4185c3b52c5eb56d6f3cc859a62958
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-08-29 04:16:54 -0300

    update source/sonixel/game/objects/common/Zone.hx

--- source/sonixel/game/objects/common/Zone.hx
+++ source/sonixel/game/objects/common/Zone.hx
@@ -5,11 +5,24 @@ import flixel.FlxSprite;
 import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
 
 import sonixel.backend.Paths;
+import sonixel.game.objects.common.tilesets.Block;
 
 /**
  * WIPPPPP
  * -aly ant
  */
+typedef GroundAngles = { //in da hitbox image
+	var posX:Float;
+	var posY:Float;
+	var angle:Float;
+}
+typedef ZoneBlock={
+	var type:String;
+	var positionX:Float;
+	var positionY:Float;
+	var groundSpeed:Float;
+	var groundAngles:Array<GroundAngles>;
+}
 
 class Zone extends FlxTypedSpriteGroup<FlxBasic>
 {
@@ -17,5 +30,9 @@ class Zone extends FlxTypedSpriteGroup<FlxBasic>
 	public var background:FlxTypedSpriteGroup<FlxBasic>;
 	public var foreground:FlxTypedSpriteGroup<FlxBasic>;
 
-	public var cloudsScroll:Float = 0.5;
+	public var terrains:FlxTypedSpriteGroup<Block>;
+
+	public function new(zone:String = ''){
+		
+	}
 }
