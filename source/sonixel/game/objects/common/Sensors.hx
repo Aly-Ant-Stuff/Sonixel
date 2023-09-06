ffb2cc33d4bae7c2db8fc23b0f2d1f143fad43ce
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-09-06 01:01:34 -0300

    ja volto galera vou bater punhe

---
+++ source/sonixel/game/objects/common/Sensors.hx
@@ -0,0 +1,44 @@
+package sonixel.game.objects.common;
+
+class Sensors extends FlxTypedSpriteGroup<FlxBasic>
+{
+	public var sensorA:FlxSprite;
+	public var sensorB:FlxSprite;
+	public var sensorC:FlxSprite;
+	public var sensorD:FlxSprite;
+	public var sensorE:FlxSprite;
+	public var sensorF:FlxSprite;
+
+	private var originSpr:FlxSprite;
+	var vertices:FlxTypedSpriteGroup<FlxBasic> = new FlxTypedSpriteGroup<FlxBasic>();
+
+	public function new(player:Player, x:Float, y:Float, initialHeightSize:Int = 16, initialWidthSize:Int = 8){
+		
+	}
+
+	public function createSensor(origin:FlxSprite, x, y, size, color, mode:String):FlxSprite{
+		var result:FlxSprite = new FlxSprite(x,y);
+
+		var width:Int=(mode == 'width' || mode == null ? size : 1);
+		var height:Int=(mode == 'height' ? size : 1);
+
+		/*
+			//HARDCODED BECAUSE IM DUMBASS :sun_glasses:
+			switch(mode){
+				case 'horizontal':
+					width = size;
+				case 'vertical':
+					height = size;
+			}
+		*/
+		result.makeGraphic(width, height, color);
+		if (mode == 'width' || mode ==null){
+			add(new FlxSprite(result.x, result.y).makeGraphic(1,1,0xFFffffff));
+			add(new FlxSprite(result.x+result.width, result.y).makeGraphic(1,1,0xFFffffff));
+		}
+		if (mode == 'height' || mode ==null){
+			add(new FlxSprite(result.x, result.y).makeGraphic(1,1,0xFFffffff));
+			add(new FlxSprite(result.x, result.y+result.height).makeGraphic(1,1,0xFFffffff));
+		}
+	}
+}
