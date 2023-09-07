6aca4c6e5e55289e7b0515d4632093e7becfea93
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-09-07 01:35:08 -0300

    update source/sonixel/game/objects/common/Sensors.hx

--- source/sonixel/game/objects/common/Sensors.hx
+++ source/sonixel/game/objects/common/Sensors.hx
@@ -13,15 +13,27 @@ class Sensors extends FlxTypedSpriteGroup<FlxBasic>
 	var vertices:FlxTypedSpriteGroup<FlxBasic> = new FlxTypedSpriteGroup<FlxBasic>();
 
 	public function new(player:Player, x:Float, y:Float, initialHeightSize:Int = 16, initialWidthSize:Int = 8){
-		
+		originSpr = new FlxSprite(player.origin.x, player.origin.y).loadGraphic(Paths.image('originSpr'));
+		add(originSpr);
+
+		sensorA = createSensor();
+		add(sensorA);
+		sensorB = createSensor();
+		add(sensorB);
+		sensorC = createSensor();
+		add(sensorC);
+		sensorD = createSensor();
+		add(sensorD);
+		sensorE = createSensor();
+		add(sensorE);
+		sensorF = createSensor();
+		add(sensorF);
 	}
 
-	public function createSensor(x, y, size, color, mode:String):FlxSprite{
+	public function createSensor(?x, ?y, ?size, ?color, mode:Null<String> = null):FlxSprite{
 		var result:FlxSprite = new FlxSprite(x,y);
-
 		var width:Int=(mode == 'width' || mode == null ? size : 1);
 		var height:Int=(mode == 'height' ? size : 1);
-
 		/*
 			//HARDCODED PORQUE EU TENHO CAPACIDADES MENTAIS MUITO LIMITADAS :sun_glasses:
 			switch(mode){
@@ -32,12 +44,14 @@ class Sensors extends FlxTypedSpriteGroup<FlxBasic>
 			}
 		*/
 		result.makeGraphic(width, height, color);
+		result.alpha = 0.3;
+		result.scrollFactor.set();
 		if (mode == 'width' || mode ==null){
-			add(new FlxSprite(result.x, result.y).makeGraphic(1,1,0xFFffffff));
-			add(new FlxSprite(result.x+result.width, result.y).makeGraphic(1,1,0xFFffffff));
+			add(new FlxSprite(result.x-1, result.y).makeGraphic(1,1,0xFFffffff));
+			add(new FlxSprite(result.x+(result.width+1), result.y).makeGraphic(1,1,0xFFffffff));
 		}
-		if (mode == 'height' || mode ==null){
-			add(new FlxSprite(result.x, result.y).makeGraphic(1,1,0xFFffffff));
+		if (mode == 'height'){
+			add(new FlxSprite(result.x, result.y-1).makeGraphic(1,1,0xFFffffff));
 			add(new FlxSprite(result.x, result.y+result.height).makeGraphic(1,1,0xFFffffff));
 		}
 		return result;
